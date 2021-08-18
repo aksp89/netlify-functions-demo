cf5a719c84230e99fe6f4415cadeef097d545167
Ankur <aksp89@gmail.com>
2021-08-18 15:00:39 +0530

    send grid post api ready

--- functions/sendgrid.js
+++ functions/sendgrid.js
@@ -2,13 +2,14 @@ const sendGrid = require("@sendgrid/mail");
 sendGrid.setApiKey("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");
 
 exports.handler =  async (event, context, callback) => {
-
+let data = JSON.parse(event.body);
+let {email, message} = data;
     try {
 await sendGrid.send({
-to: "aksp89@gmail.com",
+to: email,
 from: "ankur@kapslock.in",
 subject: "testing send grid",
-text: "hello from kapslock"
+text: message
 });
 return {
     statusCode: 200,

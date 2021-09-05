8c3e73741d537694d31f2ba85ee6cdbb8d50151c
Ankur <aksp89@gmail.com>
2021-09-05 13:17:42 +0530

    checking smtp kapslock

--- functions/mail.js
+++ functions/mail.js
@@ -1,10 +1,11 @@
 const mailer = require("nodemailer");
 const transporter = mailer.createTransport({
-    service: "Gmail",
-    port: 587,
+    host: "smtp.kapslock.in",
+    port: 465,
+    secure: true,
     auth: {
-      user: "aksp89@gmail.com",
-        pass: "GmailAK@19/06/89."
+      user: "ankur@kapslock.in",
+        pass: "a190689k"
     },
 
     tls: {
@@ -14,6 +15,7 @@ const transporter = mailer.createTransport({
 });
 
 exports.handler = function (event, context, callback) {
+    /*
     transporter.verify(function(error, success) {
         if (error) {
              console.log(error);
@@ -21,9 +23,10 @@ exports.handler = function (event, context, callback) {
              console.log('Server is ready to take our messages');
         }
      });
+*/
 
 transporter.sendMail({
-    from: "Ankur",
+    from: "Ankur@kapslock.in",
     to: "aksp89@gmail.com",
     subject: "Test mail",
     text: "test mail"
@@ -32,7 +35,7 @@ if(err) {
     console.log(err);
     callback(null, {
         statusCode: 400,
-        body: "Mail not sent"
+        body: `error: ${err}`
     });
 } else {
     console.log("mail sent");
